6194ea3153c42d020dda777f56c558c5b980569b
Souréna Par <sourena@Sourenas-MacBook-Pro.local>
2022-01-28 15:58:26 +0100

    Linter/prettier le code  functionsToTest.js & index.html

--- public/js/functionsToTest.js
+++ public/js/functionsToTest.js
@@ -1,24 +1,28 @@
 const returnAnObject = (...args) => {
-  let response = {};
+  let response = {}
   if (args.length) {
-    let index = 0;
-    args.forEach(arg => {
-      response[index] = arg;
-      index++;
+    let index = 0
+    args.forEach((arg) => {
+      response[index] = arg
+      index++
     })
   } else {
-    response = 'No argument was given to the function.';
-  };
-  return response;
-};
+    response = 'No argument was given to the function.'
+  }
+  return response
+}
+returnAnObject()
 
 const multiplyAllByTwo = (arrayOfNumbers) => {
-  let response;
-  if (arrayOfNumbers.constructor.prototype === new Array().constructor.prototype) {
-    response = arrayOfNumbers.map((val => val * 2));
-    console.log('arrayTimesTwo: ', arrayTimesTwo);
+  let response
+  if (
+    arrayOfNumbers.constructor.prototype === new Array().constructor.prototype
+  ) {
+    response = arrayOfNumbers.map((val) => val * 2)
+    console.log('arrayTimesTwo: ', arrayTimesTwo)
   } else {
-    response = 'The argument is not an Array of numbers';
-  };
-  return response;
-};
+    response = 'The argument is not an Array of numbers'
+  }
+  return response
+}
+multiplyAllByTwo()
