4972e4621955f015827789aff0f3252f486474d1
Souréna Par <sourena@Sourenas-MacBook-Pro.local>
2022-01-29 19:56:00 +0100

    Linter le code & Embellir le code fetchPokemonAbilities.mjs

--- public/js/fetchPokemonAbilities.mjs
+++ public/js/fetchPokemonAbilities.mjs
@@ -1,18 +1,16 @@
 export const fetchPokemonAbilities = async () => {
-    const pokedexNum = Math.floor(Math.random() * 266)
-    let foundAbilities = ''
-    let jsonAbilities = {}
-    let abilityToDisplay = ''
+  const pokedexNum = Math.floor(Math.random() * 266)
+  let foundAbilities = ''
 
-    try {
-      foundAbilities = await fetch(
-        `https://pokeapi.co/api/v2/ability/${pokedexNum}`,
-        { method: 'GET', headers: { 'Content-Type': 'application/json' } }
-      )
-    } catch (error) {
-      console.error(error.message)
-    }
-    console.log(foundAbilities)
-    const abilities = await foundAbilities.json()
-    return abilities
+  try {
+    foundAbilities = await fetch(
+      `https://pokeapi.co/api/v2/ability/${pokedexNum}`,
+      { method: 'GET', headers: { 'Content-Type': 'application/json' } }
+    )
+  } catch (error) {
+    console.error(error.message)
+  }
+  console.log(foundAbilities)
+  const abilities = await foundAbilities.json()
+  return abilities
 }
