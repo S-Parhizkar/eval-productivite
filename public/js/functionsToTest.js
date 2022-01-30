afc642306c430f36f436a57b697b13eebac6af71
Souréna Par <sourena@Sourenas-MacBook-Pro.local>
2022-01-30 15:01:14 +0100

    Documenter le code functionsToTest.js

--- public/js/functionsToTest.js
+++ public/js/functionsToTest.js
@@ -1,3 +1,8 @@
+/**
+ * @description theses 2 functions are spouse to bet tested by jets UT
+ * @param {any} args We can put any type as object in argument
+ * @return {object} it should return an object fil up of any type put in argument. if args is empty, it will enter in ELSE and return the no valid message 
+ */
 export const returnAnObject = (...args) => {
   let response = {}
   if (args.length) {
@@ -12,6 +17,12 @@ export const returnAnObject = (...args) => {
   return response
 }
 
+/**
+ *
+ *
+ * @param {arrayOfNumbers} arrayOfNumbers receive the numbers as array type, if nothings put in here it will enter in ELSE and return the no valid message
+ * @return {arrayOfNumbers} [number] that we put as arrayOfNumbers arguments
+ */
 export const multiplyAllByTwo = (arrayOfNumbers) => {
   let response
   if (Array.isArray(arrayOfNumbers)) {
